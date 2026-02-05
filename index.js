288c43121319184a80d4958b0280b8752cef4d43
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-02-05 19:05:54 +0500

    Started working on Password Generator

--- index.js
+++ index.js
@@ -1,32 +1,6 @@
-let score_el_home=document.getElementById("score-home")
-let score_el_guest=document.getElementById("score-guest")
-
-let counter_home=0
-let counter_guest=0
-
-function incrementOne_home(){
-    counter_home+=1
-    score_el_home.innerText=counter_home
-}
-function incrementTwo_home(){
-    counter_home+=2
-    score_el_home.innerText=counter_home
-}
-function incrementThree_home(){
-    counter_home+=3
-    score_el_home.innerText=counter_home
-}
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
 
-function incrementOne_guest(){
-    counter_guest+=1
-    score_el_guest.innerText=counter_guest
-}
-function incrementTwo_guest(){
-    counter_guest+=2
-    score_el_guest.innerText=counter_guest
-}
-function incrementThree_guest(){
-    counter_guest+=3
-    score_el_guest.innerText=counter_guest
-}
+
+
