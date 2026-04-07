b2e59709c5e5648dc95b1370189a49d3bd483a30
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-04-07 17:06:22 +0500

    Woking with listners

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-let button=document.getElementsByName("true")
-let b=button[0]
-b.setAttribute("name","asjad")
-console.log(b)
+// let button=document.getElementsByName("true")
+// let b=button[0]
+// b.setAttribute("name","asjad")
+// console.log(b)
+
+let but=document.getElementById("but")
+but.addEventListener("click",()=>{
+    but.textContent=but.textContent.toUpperCase()
+})
+
+
