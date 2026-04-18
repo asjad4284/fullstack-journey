776bec865c20d2acab1884120462c00db3b0db66
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-04-18 17:58:44 +0500

    Prototype Chain

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function newUser(name,score){
-    const user={}
+    const user=Object.create(functionstore)
     user.name=name
     user.score=score
-    user.increment=function(){
-        user.score++
-    }
     return user
+}
+const functionstore={
+    increment:function(){
+        this.score++
+    }
 }
 
 const user1=newUser("Corey",5)
 const user2=newUser("John",10)
 
+user1.increment()
 user1.increment()
 console.log(user1.score)
