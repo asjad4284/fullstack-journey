0e51a3cbc7c781a01ace9deba3699f1e8a4265fb
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-04-24 21:51:38 +0500

    Working with object properties

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function newUser(name,score){
-    const user=Object.create(functionstore)
+    const user=Object.create(userFunction)
     user.name=name
     user.score=score
     return user
 }
 
-const functionstore={
+const userFunction={
     increment:function(){
         this.score++
     }
 }
 
+
 const user1=newUser("Corey",5)
 const user2=newUser("John",10)
 
 user1.increment()
-user1.increment()
+user2.increment()
+
 console.log(user1.score)
+console.log(user2.score)
+console.log(user1.hasOwnProperty("score"))
