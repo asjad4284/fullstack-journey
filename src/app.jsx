8a52010b90e62ffbf72a1d7844c05f0899b9ad4a
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-05-03 20:14:15 +0500

    Setting the proxy to work the API

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,23 @@
 import ReactDOM from "react-dom/client";
 import Pizza from "./pizza";
 
-const App=()=>{
-    return (
-        <div>
-            <h1>Padre Gino's</h1>
-            <Pizza name="Pepperoni" description="pep,cheese n stuff"/>
-            <Pizza name="Americano" description="hot dogs,french fries n stuff"/>
-        </div>
-        
-    )
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Padre Gino's</h1>
+      <Pizza
+        name="Pepperoni"
+        description="pep,cheese n stuff"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="Americano"
+        description="hot dogs,french fries n stuff"
+        image="/public/pizzas/big_meat.webp"
+      />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App/>);
+root.render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  // Setting the proxy for api server
+  // To use the API, we first run the api server in other terminal
+  
   server: {
     proxy: {
       "/api": {
