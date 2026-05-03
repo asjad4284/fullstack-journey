eaf973896af8c3d2d56dd7e14f386b1432210129
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-05-03 17:17:47 +0500

    Work with core concepts

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import Pizza from "./pizza";
+
+const App=()=>{
+    return (
+        <div>
+            <h1>Padre Gino's</h1>
+            <Pizza name="Pepperoni" description="pep,cheese n stuff"/>
+            <Pizza name="Americano" description="hot dogs,french fries n stuff"/>
+        </div>
+        
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App/>);
