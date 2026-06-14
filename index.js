66760ab8388a88544bbbe7415ba8bdc439c62cdf
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-06-15 01:46:07 +0500

    The new keyword

--- index.js
+++ index.js
@@ -2,16 +2,4 @@ function ask(question){
     console.log(this.teacher,question)
 }
 
-var workshop1={
-    teacher:"Corey",
-    ask:ask,
-};
-
-
-var workshop2={
-    teacher:"John",
-    ask:ask,
-};
-
-workshop1.ask("How can we solve this problem!")
-workshop2.ask("How can we solve this problem!")
+var emptyObject=new ask("This is new object")
