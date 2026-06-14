d0568f22592a6ce2d570ceb723d7a9ec24ee329a
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-06-15 01:27:11 +0500

    The This keyword

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-kljkjljkl
+function ask(question){
+    console.log(this.teacher,question)
+}
+
+function otherFunction(){
+    var context={
+        teacher:"John",
+    };
+    ask.call(context,"Why is this happening?")
+}
+
+otherFunction()
