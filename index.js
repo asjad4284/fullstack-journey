00aa5d452f25e220a95a0aeee37e89758826d94a
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-06-16 23:43:03 +0500

    Hoisting

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function Workshop(teacher){
-    this.teacher=teacher
-}
-
-Workshop.prototype.ask=function(question){
-    console.log(this.teacher,question)
-}
-
-let deepJs=new Workshop("Corey")
-
-deepJs.ask("Can u teach me fundamentals of FastApi?")
+a=5;
+var a;
+console.log(a)
