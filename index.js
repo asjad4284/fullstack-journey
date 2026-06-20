325e8e4e2cf2fa6ba3cdbde971b78d39d24fc33c
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-06-20 14:24:32 +0500

    Return next element with a function

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-function createFunction(){
-    function add2(num){
-        return num+2;
+function createFunction(array){
+    let i=0;
+    function inner(){
+        let element=array[i]
+        i++;
+        return element
     }
-    return add2
+    return inner
 }
 
-const newFunction=createFunction()
-const result=newFunction(3)
-console.log(result)
+const returnNextElement=createFunction([4,5,6])
+const element1=returnNextElement()
+const element2=returnNextElement()
+console.log(element1)
+console.log(element2)
