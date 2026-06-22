fcf69622f5f8c96f0fb4af43b50b3a0dd555184f
Asjad Sajjad <70141623@student.uol.edu.pk>
2026-06-22 19:40:58 +0500

    Async generatos

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 function* createFlow(){
-    const num=10;
-    const newNum=yield num;
-    yield 5+newNum;
-    yield 8
+    const res=yield fetch("https;//twitter.com/will/1")
+    console.log(res)
+}
+function printHello(value){
+    returnNextElement.next(value)
 }
 
 const returnNextElement=createFlow()
-const element1=returnNextElement.next().value
-const element2=returnNextElement.next(2).value
-const element3=returnNextElement.next().value
-console.log(element1)
-console.log(element2)
-console.log(element3)
+const futureData=returnNextElement.next()
+futureData.then(printHello)
